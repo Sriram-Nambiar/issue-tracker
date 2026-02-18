dc371efe516fef0fafd3ed6b4651d04fded4f474
Sriram-Nambiar <sriramnambiar21@gmail.com>
2026-02-18 12:40:40 +0530

    Set up Radix UI

---
+++ app/issues/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Button} from "@radix-ui/themes";
+function issues() {
+  return (
+    <div>
+      <Button>Create Issue</Button>
+    </div>
+  )
+}
+
+export default issues
